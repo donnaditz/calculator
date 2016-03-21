fcc86bbfee2b33779319503aa4f2186289cee435
Gigi Ditz and Kate Hall <gigi@>
2016-03-21 16:36:27 -0700

    add gallon conversion exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-var temp=function(number1){
-  return ((5/9) * (number1-32));
-  };
-var tempinput=prompt("What is the temp in F ?");
-alert(temp(tempinput));
+var conversion = function(number) {
+return(number/.26417);
+};
+
+// user interface
+// var userinput = prompt("imput gallons to convert to litres")
+// alert(conversion(userinput));
